fd98763cc37de83f478eec4b317f83dfa3c029ce
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-02-24 16:52:55 -0600

    args

---
+++ src/main/groovy/org/zirbes/arrrgs/PirateShip.groovy
@@ -0,0 +1,12 @@
+package org.zirbes.arrrgs
+
+class PirateShip {
+
+    static void main(String[] argv) {
+        println "Arrrrg, I found the following arrrgs!"
+        argv.each{ arg ->
+            println " * ${arg}"
+        }
+        println "Shiver my timbers, that'd be all."
+    }
+}
